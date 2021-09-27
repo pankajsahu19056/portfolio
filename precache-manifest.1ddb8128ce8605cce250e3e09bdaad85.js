56b0ed5ac2e9dbe38d5d2771acf976e72d3d70c1
pankaj sahu <pankajsahu19056@gmail.com>
2021-09-28 01:09:01 +0530

    Updates

---
+++ precache-manifest.1ddb8128ce8605cce250e3e09bdaad85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f29885feceeb5921aa7439c0f000af7",
+    "url": "/pankajsahu19056/portfolio/index.html"
+  },
+  {
+    "revision": "cf9287704a3d801ac08d",
+    "url": "/pankajsahu19056/portfolio/static/css/main.ede1edec.chunk.css"
+  },
+  {
+    "revision": "ef02ea6d01ef814393ad",
+    "url": "/pankajsahu19056/portfolio/static/js/2.b06002cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pankajsahu19056/portfolio/static/js/2.b06002cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9287704a3d801ac08d",
+    "url": "/pankajsahu19056/portfolio/static/js/main.e6043138.chunk.js"
+  },
+  {
+    "revision": "ef2fe9fb70eec7c6f513",
+    "url": "/pankajsahu19056/portfolio/static/js/runtime-main.648d9b80.js"
+  },
+  {
+    "revision": "456d27d30afacf5ef9c2d04e25452c99",
+    "url": "/pankajsahu19056/portfolio/static/media/mock1.456d27d3.png"
+  },
+  {
+    "revision": "37e11540fe7357463bd40d3effb35da1",
+    "url": "/pankajsahu19056/portfolio/static/media/mock2.37e11540.png"
+  },
+  {
+    "revision": "6a736fcd570ebcf9699f8d5e9f949f60",
+    "url": "/pankajsahu19056/portfolio/static/media/mock3.6a736fcd.png"
+  },
+  {
+    "revision": "bd6a383d15853619853b9eba50524095",
+    "url": "/pankajsahu19056/portfolio/static/media/mock4.bd6a383d.png"
+  },
+  {
+    "revision": "6eaa70fb36a1281a548de4097ef29c89",
+    "url": "/pankajsahu19056/portfolio/static/media/self.6eaa70fb.png"
+  }
+]);
